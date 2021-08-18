b539001bef50be4985b4a5f591550e78c99d31c3
Firas Rafislam <firasrafislam@live.com>
2021-08-19 05:23:29 +0700

    fixes data-text

--- src/components/docs-sidebar-header-section.js
+++ src/components/docs-sidebar-header-section.js
@@ -4,13 +4,13 @@ import DocsNavLogoLockup from "./docs-nav-logo-lockup"
 
 const DocsSidebarHeaderSection = () => (
   <div className="DocsSidebar--section DocsSidebar--header-section">
-    <a className="DocsSidebar--cloudflare-logo-link DocsSidebar--link" href="https://duckduckgo.com">
+    <a className="DocsSidebar--cloudflare-logo-link DocsSidebar--link" href="/">
       <DocsNavLogoLockup
         text={(
           <>
-            <span data-text="Cloudflare">Bad Guy</span>
+            <span data-text="Bad Guy">Bad Guy</span>
             <span>&nbsp;</span>
-            <span data-text="Docs">Express</span>
+            <span data-text="Express">Express</span>
           </>
         )}
       />
